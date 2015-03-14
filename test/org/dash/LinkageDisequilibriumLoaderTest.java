ca67464b96355188126eafa735eeb31e0eefc691
mpresteg <mpresteg@gmail.com>
2015-03-13 23:15:33 -0500

    Switched back inlineGLString test to use MultilocusUnphasedGenotype (mug) after repairing allele duplication logic

--- test/org/dash/LinkageDisequilibriumLoaderTest.java
+++ test/org/dash/LinkageDisequilibriumLoaderTest.java
@@ -2,7 +2,6 @@ package org.dash;
 
 import static org.junit.Assert.assertNotNull;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.dash.valid.LinkageDisequilibriumLoader;
@@ -31,10 +30,7 @@ public class LinkageDisequilibriumLoaderTest {
 	@Test
 	public void testLinkageReportingInlineGLString() {
 		String fullyQualified = GLStringUtilities.fullyQualifyGLString("HLA-A*02:01:01:01+HLA-A*24:02:01:01/24:02:01:03^HLA-B*27:02:01+HLA-B*35:02:01/35:211^HLA-C*02:02:02+HLA-C*04:01:01:01/04:01:01:02/04:01:01:03/04:01:01:04/04:01:01:05/04:20/04:117^HLA-DPA1*01:03:01:01/01:03:01:02/01:03:01:03/01:03:01:04/01:03:01:05+HLA-DPA1*02:01:01^HLA-DPB1*04:01:01:01/04:01:01:02+HLA-DPB1*17:01^HLA-DQA1*02:01+HLA-DQA1*05:05:01:01/05:05:01:02/05:05:01:03/05:09/05:11^HLA-DQB1*02:02+HLA-DQB1*03:01:01:01/03:01:01:02/03:01:01:03^HLA-DRB1*07:01:01:01/07:01:01:02+HLA-DRB1*11:01:01^HLA-DRB3*01:01:02:01/01:01:02:02^HLA-DRB4*01:03:01:01/01:03:01:03");
-//		MultilocusUnphasedGenotype mug = GLStringUtilities.convertToMug(fullyQualified);
-//		LinkageDisequilibriumLoader.detectLinkages(mug);
-		List<String> glStrings = new ArrayList<String>();
-		glStrings.add(fullyQualified);
-		LinkageDisequilibriumLoader.detectLinkages(glStrings);
+		MultilocusUnphasedGenotype mug = GLStringUtilities.convertToMug(fullyQualified);
+		LinkageDisequilibriumLoader.detectLinkages(mug);
 	}
 }
