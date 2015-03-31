6d93ec1b1ccae521e86e2220d926b3f3246dc38c
mpresteg <mpresteg@gmail.com>
2015-03-31 01:00:22 -0500

    Modified compareTo so that it behaves consistently with equals - resolved a bug in which elements with the same number of 'hits' were seen as identical, even if the contents were different.

--- src/org/dash/valid/DisequilibriumElementComparator.java
+++ src/org/dash/valid/DisequilibriumElementComparator.java
@@ -10,10 +10,12 @@ public class DisequilibriumElementComparator implements Comparator<Object> {
 	public int compare(Object element1, Object element2) {
 		if (element1 instanceof DisequilibriumElementByRace && element2 instanceof DisequilibriumElementByRace) {
 			// those with more linkages should sort first
-			return ((DisequilibriumElementByRace)element2).getFrequenciesByRace().size() - ((DisequilibriumElementByRace)element1).getFrequenciesByRace().size();
+			int ret = ((DisequilibriumElementByRace)element2).getFrequenciesByRace().size() - ((DisequilibriumElementByRace)element1).getFrequenciesByRace().size();
+			if (ret != 0) {
+				return ret;
+			}
 		}
 		
 		// else sort alphabetically
-		return element1.toString().compareTo(element2.toString());
-	}
+		return element1.toString().compareTo(element2.toString());	}
 }
