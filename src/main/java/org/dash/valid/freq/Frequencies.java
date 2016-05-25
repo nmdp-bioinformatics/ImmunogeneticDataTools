ff79d91670f3735309d6f7f0b25d90ead712e51e
Mike Halagan <mhalagan@nmdp.org>
2016-05-25 11:11:52 -0500

    Merged conflicting pull requests.

---
+++ src/main/java/org/dash/valid/freq/Frequencies.java
@@ -0,0 +1,40 @@
+package org.dash.valid.freq;
+
+import java.util.logging.Logger;
+
+public enum Frequencies {
+	NMDP ("nmdp"), NMDP_STD ("nmdp-std"), NMDP_2007 ("nmdp-2007"), WIKIVERSITY ("wiki");
+	
+	private String shortName;
+	public static final String FREQUENCIES_PROPERTY = "org.dash.frequencies";
+	
+	private static final Logger LOGGER = Logger.getLogger(Frequencies.class.getName());
+	
+	private Frequencies(String shortName) {
+		this.shortName = shortName;
+	}
+	
+	public String getShortName() {
+		return this.shortName;
+	}
+	
+	private static Frequencies getDefault() {
+		return NMDP_2007;
+	}
+	
+	public static Frequencies lookup(String shortName) {
+		for (Frequencies freq : values()) {
+			if (freq.getShortName().equals(shortName)) {
+				return freq;
+			}
+		}
+		
+		LOGGER.warning("The specified frequencies: " + shortName + " are not supported.  Defaulting to : " + getDefault());
+		return Frequencies.getDefault();
+	}
+	
+	@Override
+	public String toString() {
+		return getShortName();
+	}
+}
